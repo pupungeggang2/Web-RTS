dbf685720ab491de265f25966b11d78e925240a7
pupungeggang2 <kyongkyong2892@gmail.com>
2023-12-26 02:51:00 +0900

    0.0.0

--- Script/Main.js
+++ Script/Main.js
@@ -3,11 +3,57 @@ window.onerror = errorHandle
 window.oncontextmenu = rightClick
 
 function main() {
+    canvas = document.getElementById('Screen')
+    context = canvas.getContext('2d')
+
+    window.addEventListener('mouseup', mouseUp, false)
+    window.addEventListener('mousedown', mouseDown, false)
+    window.addEventListener('keydown', keyDown, false)
+    window.addEventListener('keyup', keyUp, false)
+
+    imageLoad()
+
+    gameFrameCurrent = Date.now()
+    gameFramePrevious = Date.now() - 16
     
+    gameInstance = requestAnimationFrame(loop)
+}
+
+function loop() {
+    gameFrameCurrent = Date.now()
+
+    if (scene === 'Title') {
+        loopTitle()
+    } else if (scene === 'LevelSelect') {
+        loopLevelSelect()
+    } else if (scene === 'Game') {
+        loopGame()
+    }
+
+    gameFramePrevious = Date.now()
+    gameInstance = requestAnimationFrame(loop)
+}
+
+function mouseUp(event) {
+
+}
+
+function mouseDown(event) {
+
+}
+
+function keyDown(event) {
+
+}
+
+function keyUp(event) {
+
 }
 
 function errorHandle(err, url, line, col, obj) {
-
+    if (obj != null) {
+        cancelAnimationFrame(gameInstance)
+    }
 }
 
 function rightClick() {
